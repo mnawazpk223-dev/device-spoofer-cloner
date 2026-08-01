261a5d2a68f0af1e265a5828b690c5f760dd78c2
mnawazpk223-dev <m.nawazpk223@gmail.com>
2026-08-01 15:50:36 +0300

    Set content view in MainActivity on creation

--- app/src/main/java/com/virtual/cloner/MainActivity.java
+++ app/src/main/java/com/virtual/cloner/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         Toast.makeText(this, "App Started Successfully!", Toast.LENGTH_SHORT).show();
     }
 }
