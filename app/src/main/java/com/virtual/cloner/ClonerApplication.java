4204693b109eaacbe8e1bb5176c9d2bcea867fee
mnawazpk223-dev <m.nawazpk223@gmail.com>
2026-08-01 15:05:15 +0300

    Implement ClonerApplication with BlackBoxCore integration

---
+++ app/src/main/java/com/virtual/cloner/ClonerApplication.java
@@ -0,0 +1,29 @@
+package com.virtual.cloner;
+
+import android.app.Application;
+import android.content.Context;
+import top.niunaijun.blackbox.BlackBoxCore;
+import top.niunaijun.blackbox.app.configuration.ClientConfiguration;
+
+public class ClonerApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        try {
+            BlackBoxCore.get().doAttachBaseContext(base, new ClientConfiguration() {
+                @Override
+                public String getHostPackageName() {
+                    return base.getPackageName();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        BlackBoxCore.get().doCreate();
+    }
+}
